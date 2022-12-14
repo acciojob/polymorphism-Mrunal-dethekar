45fde7681c56cc478b1cfccaf1cb13c8bf7a757a
Mrunal-dethekar <95629099+Mrunal-dethekar@users.noreply.github.com>
2022-12-14 16:13:00 +0530

    poly

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,20 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+    }
+
+    Product p = new Product();
+    int ans1 = p.product(1,2);
+    int ans2 = p.product(1,2,3);
+    double ans3 = p.product(1.5,2.5);
 }
